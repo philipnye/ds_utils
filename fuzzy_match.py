0ef4b05e694f51a4854b3418637a98bd74093bef
Philip Nye <philipnye@users.noreply.github.com>
2023-09-26 20:06:37 +0100

    - Refactor fuzzy_match(), fuzzy_merge() to handle MultiIndexes - Implement drop_na arg - Rename threshold arg - Implement test_drop_na_false()

--- fuzzy_match.py
+++ fuzzy_match.py
@@ -13,9 +13,10 @@ def fuzzy_match(
     df_right: pd.DataFrame,
     column_left: Hashable,
     column_right: Hashable,
-    threshold: int = 90,
+    score_cutoff: int = 90,
     limit: int = 1,
-    clean_strings: bool = True
+    clean_strings: bool = True,
+    drop_na: bool = True,
 ) -> pd.DataFrame:
     '''
         Fuzzy match two dataframes.
@@ -26,98 +27,77 @@ def fuzzy_match(
                 - df_right: The dataframe in which we want to look for matches
                 to values in df_left
                 - column_left, column_right: Columns on which to match
-                - threshold: A score below which any matches
+                - score_cutoff: A score below which any matches
                 will be dropped
                 - limit: The number of matches to find for each row
                 in df_left
                 - clean_strings: Whether to apply rapidfuzz's default_process
                 processor, which converts strings to lowercase, removes
                 non-alphanumeric characters and trims whitespace
+                - drop_na: Whether to drop rows where no matches are found
 
             Returns:
                 - df_matches: A dataframe of matches with a MultiIndex
                 consisting of the ids from df_left and df_right, and
-                columns match_string, match_score
+                columns match_string, match_score. Where df_left or
+                df_right has a MultiIndex, the relevant index is a tuple
 
             Notes:
+                - This adds matches as rows rather than columns, to ensure a
+                tidy dataset
                 - The maximum number of matches that can be returned is
                 len(df_left) * limit
-                - This adds matches as rows rather than columns, to preserve a
-                tidy dataset
-                - Passing a series to process.extract() yields results in the
-                form [(<value>, <score>, <index>), ...].
-                (Ref: https://stackoverflow.com/a/63725864/4659442)
     '''
 
     # Create a series of matches
-    # Creates a series with id from df_left and column name _column_left_,
-    # with _limit_ matches per item
+    # NB: Passing a series to process.extract() yields a series named column_left
+    # where the index is the index of df_left and the values are lists of tuples,
+    # of the form [(<value>, <score>, <index>), ...] - in this case the match
+    # value from df_right, the match score and index of df_right. Where df_right
+    # has a MultiIndex, the index is a tuple
+    # (ref: https://stackoverflow.com/a/63725864/4659442)
     series_matches = df_left[column_left].apply(
         lambda x: process.extract(
-            x, df_right[column_right], limit=limit,
+            x, df_right[column_right],
+            limit=limit, score_cutoff=score_cutoff,
             processor=utils.default_process if clean_strings else None
         )
     )
 
-    # Convert matches to a tidy dataframe
+    # Drop empty matches
+    if drop_na:
+        series_matches = series_matches[
+            series_matches.apply(lambda x: len(x) > 0)
+        ]
+
+    # Convert matches to a dataframe in long form
     df_matches = series_matches.to_frame()
-    df_matches = df_matches.explode(column_left)     # Convert list of matches to rows
+    df_matches.index.name = 'df_left_id'
+    df_matches = df_matches.explode(column_left)
 
     # Convert match tuple to columns
-    if df_right.index.nlevels == 1:
-        df_matches[
-            ['match_string', 'match_score', 'df_right_id']
-        ] = pd.DataFrame(
-            df_matches[column_left].tolist(), index=df_matches.index
-        )
-
-    # Handle case where df_right has a MultiIndex
-    # In this case, df_matches should have columns
-    # ['match_string', 'match_score', 'df_right_<index_level1_name>',
-    # 'df_right_<index_level2_name>', ...]
-    else:
-        df_matches[
-            ['match_string', 'match_score', 'df_right_indexes']
-        ] = pd.DataFrame(
-            df_matches[column_left].tolist(), index=df_matches.index
-        )
-        # Convert index tuple to columns. NB: Creation of columns is done in two stages as list
-        # comprehension can't be combined with other column-creation operations
-        df_matches = pd.concat([
-            df_matches,
-            pd.DataFrame(
-                df_matches['df_right_indexes'].tolist(),
-                columns=[
-                    'df_right_' + index_column
-                    for index_column in df_right.index.names
-                ],
-                index=df_matches.index
-            )
-        ], axis=1)
-        df_matches.drop('df_right_indexes', axis=1, inplace=True)      # Drop column of index tuples
-    df_matches.drop(column_left, axis=1, inplace=True)      # Drop column of match tuples
-
-    # Reset index, as in creating a tidy dataframe we've introduced multiple
-    # rows per id, so that no longer functions well as the index
-    if df_matches.index.name:
-        index_name = df_matches.index.name     # Stash index name
-    else:
-        index_name = 'index'        # Default used by pandas
-    df_matches.reset_index(inplace=True)
-
-    # The previous index has now become a column: rename for ease of reference
-    df_matches.rename(columns={index_name: 'df_left_id'}, inplace=True)
-
-    # Drop matches below threshold
-    df_matches.drop(
-        df_matches.loc[df_matches['match_score'] < threshold].index,
-        inplace=True
+    df_matches = pd.DataFrame(
+        index=df_matches.index,
+        data=df_matches[column_left].tolist(),
+        columns=['match_string', 'match_score', 'df_right_id']
     )
 
-    # Set index
-    # NB: This  MultiIndex will be a unique index, as long
-    # as df_left and df_right have unique indexes
-    df_matches.set_index(['df_left_id', 'df_right_id'], inplace=True)
+    # Convert indexes to tuples where df_left and/or df_right have MultiIndexes
+    # as otherwise any subsequent merging will fail
+    # NB: This is done before adding df_right_id to the index, as otherwise
+    # df_right_id would become part of df_left_id
+    # NB: Flattening df_matches index is only needed where df_left has a
+    # MultiIndex, as in the case where df_right has a MultiIndex the df_right
+    # index will have been a single, named column in df_matches
+    if df_left.index.nlevels > 1:
+        df_matches.index = pd.MultiIndex.to_flat_index(df_matches.index)
+        df_matches.index.name = 'df_left_id'
+
+    # Add df_right id to index, meaning it will consist of df_left id and
+    # df_right id
+    # NB: This will be a unique index, as long as df_left and df_right have
+    # unique indexes
+    df_matches.set_index(['df_right_id'], append=True, inplace=True)
 
     return df_matches
 
@@ -128,28 +108,41 @@ def fuzzy_merge(
     df_right: pd.DataFrame,
     column_left: Hashable,
     column_right: Hashable,
-    threshold: int = 90,
-    limit: int = 1
+    score_cutoff: int = 90,
+    limit: int = 1,
+    clean_strings: bool = True,
+    drop_na: bool = True
 ):
     '''
         Fuzzy merge two dataframes.
 
             Parameters:
-                - df_left, df_right: The dataframes to match
+                - df_left: The base dataframe which we want to find matches
+                for
+                - df_right: The dataframe in which we want to look for matches
+                to values in df_left
                 - column_left, column_right: Columns on which to match
-                - threshold: A score below which any matches
+                - score_cutoff: A score below which any matches
                 will be dropped
                 - limit: The number of matches to find for each row
                 in df_left
+                - clean_strings: Whether to apply rapidfuzz's default_process
+                processor, which converts strings to lowercase, removes
+                non-alphanumeric characters and trims whitespace
 
             Returns:
-                - df_matches: A dataframe of matches with columns
-                df_left_id, match_string, match_score, df_right_id
+                - df_matches: A dataframe of matches with a MultiIndex
+                consisting of the ids from df_left and df_right, and
+                columns match_string, match_score. Where df_left or
+                df_right has a MultiIndex, the relevant index is a tuple
 
             Notes:
-                - This adds matches as rows rather than columns, to preserve a
+                - This adds matches as rows rather than columns, to ensure a
                 tidy dataset
-                - Where df_right has a MultiIndex, the index is a tuple
+                - The merge carried out is a left merge, so all rows from
+                df_left are retained
+                - The maximum number of matches that can be returned is
+                len(df_left) * limit
     '''
 
     # Fuzzy match datasets
@@ -158,29 +151,37 @@ def fuzzy_merge(
         df_right,
         column_left,
         column_right,
-        threshold=threshold,
-        limit=limit
+        score_cutoff=score_cutoff,
+        limit=limit,
+        clean_strings=clean_strings,
+        drop_na=drop_na
     )
 
+    # Convert indexes to tuples where df_left and/or df_right have MultiIndexes
+    # as otherwise any subsequent merging will fail
+    # NB: We do this on copies of df_left and/or df_right, and use these in the
+    # # subsequent merge, so that we don't modify the original dataframes
+    df_left_flat_index = df_left.copy()
+    df_right_flat_index = df_right.copy()
+
+    if df_left.index.nlevels > 1:
+        df_left_flat_index.index = pd.MultiIndex.to_flat_index(df_left_flat_index.index)
+        df_left_flat_index.index.name = 'df_left_id'
+    if df_right.index.nlevels > 1:
+        df_right_flat_index.index = pd.MultiIndex.to_flat_index(df_right_flat_index.index)
+
     # Merge data
-    df_output = df_left.merge(
+    df_output = df_left_flat_index.merge(
         df_matches,
         how='left',
         left_index=True,
         right_on='df_left_id'
     ).merge(
-        df_right,
+        df_right_flat_index,
         how='left',
         left_on='df_right_id',
         right_index=True,
         suffixes=['_df_left', '_df_right']
     )
-
-    # Recreate index
-    df_output.set_index('df_left_id', inplace=True)
-
-    # Drop columns used in the matching
-    df_output = df_output[['col_a_df_left', 'col_b_df_left', 'col_b_df_right']]
-    df_output.index.name = 'id'
 
     return df_output
--- tests/test_fuzzy_match.py
+++ tests/test_fuzzy_match.py
@@ -30,7 +30,7 @@ def test_simple_case():
         df_right,
         'col_a',
         'col_a',
-        threshold=60,
+        score_cutoff=60,
         limit=2
     )
 
@@ -78,7 +78,7 @@ def test_clean_strings_false():
         df_right,
         'col_a',
         'col_a',
-        threshold=60,
+        score_cutoff=60,
         limit=2,
         clean_strings=False
     )
@@ -95,6 +95,54 @@ def test_clean_strings_false():
         data={
             'match_string': ['one', 'three', 'fours', 'five', 'five'],
             'match_score': [100.000000, 100.000000, 88.888889, 100.000000, 100.000000],
+        }
+    )
+
+    # Test output
+    pdt.assert_frame_equal(df_matches, df_expected)
+
+
+def test_drop_na_false():
+    '''
+        Test non-empty, non-MultiIndex df_left, non-empty, non-MultiIndex df_right,
+        where matches exist, drop_na=False
+    '''
+
+    # Create dataframes
+    df_left = pd.DataFrame({
+        'col_a': ['one', 'two', 'three', 'four', 'five'],
+        'col_b': [1, 2, 3, 4, 5]
+    })
+    df_right = pd.DataFrame({
+        'col_a': ['one', 'too', 'three', 'fours', 'five', 'five'],
+        'col_b': ['a', 'b', 'c', 'd', 'e', 'f']
+    })
+
+    # Use function
+    df_matches = fuzzy_match(
+        df_left,
+        df_right,
+        'col_a',
+        'col_a',
+        score_cutoff=80,
+        limit=2,
+        drop_na=False
+    )
+
+    # Add expected output
+    df_expected = pd.DataFrame(
+        index=pd.MultiIndex.from_arrays(
+            [
+                [0, 1, 2, 3, 4, 4],
+                [0.0, pd.NA, 2.0, 3.0, 4.0, 5.0],
+            ],
+            names=['df_left_id', 'df_right_id']
+        ),
+        data={
+            'match_string': ['one', pd.NA, 'three', 'fours', 'five', 'five'],
+            'match_score': pd.to_numeric(
+                [100.000000, pd.NA, 100.000000, 88.888889, 100.000000, 100.000000]
+            )
         }
     )
 
@@ -124,7 +172,7 @@ def test_column_not_in_df():
             df_right,
             'col_c',
             'col_a',
-            threshold=60,
+            score_cutoff=60,
             limit=2
         )
 
@@ -135,7 +183,7 @@ def test_column_not_in_df():
             df_right,
             'col_a',
             'col_c',
-            threshold=60,
+            score_cutoff=60,
             limit=2
         )
 
