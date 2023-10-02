e9da15714de16b88e1d8d4b5b4dd234f45b1cae7
Philip Nye <philipnye@users.noreply.github.com>
2023-10-02 06:32:12 +0100

    Implement test_function_on_iterable()

--- basic_operations.py
+++ basic_operations.py
@@ -1,4 +1,5 @@
-from typing import Iterable, Any
+# %%
+from typing import Any, Callable, Iterable
 
 
 def all_equal(iterable: Iterable[Any]) -> bool:
@@ -33,3 +34,54 @@ def all_equal(iterable: Iterable[Any]) -> bool:
     except StopIteration:
         return True
     return all(first == x for x in iterable)
+
+
+def test_function_on_iterable(
+    iterable: Iterable[Any],
+    function: Callable,
+    empty_iterable_result: bool = False,
+) -> bool:
+    '''
+    Check that all elements of an iterable are equal
+
+    Parameters
+    ----------
+    iterable: The iterable to test
+    function: The function to test
+
+    Returns
+    -------
+    result: True if the function returns True for all elements of the iterable
+
+    Examples
+    --------
+    >>> test_function_on_iterable([1, 2, 3], lambda x: x > 0)
+    True
+    >>> test_function_on_iterable([1, 2, 3], lambda x: x > 1)
+    False
+    >>> test_function_on_iterable([], lambda x: x > 0)
+    False
+    >>> test_function_on_iterable([], lambda x: x > 0, empty_iterable_result=True)
+    True
+    >>> test_function_on_iterable([[1, 2], [3], [4, 5, 6, 7]], lambda x: len(x) > 0)
+    True
+    >>> test_function_on_iterable([[1, 2], [3], [4, 5, 6, 7]], lambda x: len(x) > 1)
+    False
+
+    Notes
+    -----
+    None
+
+    '''
+    # Test for empty iterable
+    if empty_iterable_result:
+        if len(iterable) == 0:
+            return True
+    else:
+        if len(iterable) == 0:
+            return False
+
+    # Test function on iterable
+    result = [function(x) for x in iterable]
+
+    return all(result)
