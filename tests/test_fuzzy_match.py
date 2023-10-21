876ac64360ff9594e9b6615f309a47649acef0bd
Philip Nye <philipnye@users.noreply.github.com>
2023-10-21 15:11:44 +0100

    Add - test_multiindex_df_left() - test_multiindex_df_right() - test_multiindex_df_left_and_right

--- tests/test_fuzzy_match.py
+++ tests/test_fuzzy_match.py
@@ -55,6 +55,179 @@ def test_simple_case():
     return
 
 
+def test_multiindex_df_left():
+    '''
+        Test non-empty, MultiIndex df_left, non-empty, non-MultiIndex df_right,
+        where matches exist
+    '''
+
+    # Create dataframes
+    df_left = pd.DataFrame(
+        index=pd.MultiIndex.from_arrays(
+            [
+                [0, 1, 2, 3, 4],
+                [5, 6, 7, 8, 9],
+            ],
+        ),
+        data={
+            'col_a': ['one', 'two', 'three', 'four', 'five'],
+            'col_b': [1, 2, 3, 4, 5]
+        }
+    )
+
+    df_right = pd.DataFrame({
+        'col_a': ['one', 'too', 'three', 'fours', 'five', 'five'],
+        'col_b': ['a', 'b', 'c', 'd', 'e', 'f']
+    })
+
+    # Use function
+    df_matches = fuzzy_match(
+        df_left,
+        df_right,
+        'col_a',
+        'col_a',
+        score_cutoff=60,
+        limit=2
+    )
+
+    # Add expected output
+    df_expected = pd.DataFrame(
+        index=pd.MultiIndex.from_arrays(
+            [
+                [(0, 5), (1, 6), (2, 7), (3, 8), (4, 9), (4, 9)],
+                [0, 1, 2, 3, 4, 5]
+            ],
+            names=['df_left_id', 'df_right_id']
+        ),
+        data={
+            'match_string': ['one', 'too', 'three', 'fours', 'five', 'five'],
+            'match_score': [100.000000, 66.666667, 100.000000, 88.888889, 100.000000, 100.000000],
+        }
+    )
+
+    # Test output
+    pdt.assert_frame_equal(df_matches, df_expected)
+
+    return
+
+
+def test_multiindex_df_right():
+    '''
+        Test non-empty, non-MultiIndex df_left, non-empty, MultiIndex df_right,
+        where matches exist
+    '''
+
+    # Create dataframes
+    df_left = pd.DataFrame({
+        'col_a': ['one', 'two', 'three', 'four', 'five'],
+        'col_b': [1, 2, 3, 4, 5]
+    })
+    df_right = pd.DataFrame(
+        index=pd.MultiIndex.from_arrays(
+            [
+                [0, 1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10, 11],
+            ],
+        ),
+        data={
+            'col_a': ['one', 'too', 'three', 'fours', 'five', 'five'],
+            'col_b': ['a', 'b', 'c', 'd', 'e', 'f']
+        }
+    )
+
+    # Use function
+    df_matches = fuzzy_match(
+        df_left,
+        df_right,
+        'col_a',
+        'col_a',
+        score_cutoff=60,
+        limit=2
+    )
+
+    # Add expected output
+    df_expected = pd.DataFrame(
+        index=pd.MultiIndex.from_arrays(
+            [
+                [0, 1, 2, 3, 4, 4],
+                [(0, 6), (1, 7), (2, 8), (3, 9), (4, 10), (5, 11)]
+            ],
+            names=['df_left_id', 'df_right_id']
+        ),
+        data={
+            'match_string': ['one', 'too', 'three', 'fours', 'five', 'five'],
+            'match_score': [100.000000, 66.666667, 100.000000, 88.888889, 100.000000, 100.000000],
+        }
+    )
+
+    # Test output
+    pdt.assert_frame_equal(df_matches, df_expected)
+
+    return
+
+
+def test_multiindex_df_left_and_right():
+    '''
+        Test non-empty, MultiIndex df_left, non-empty, MultiIndex df_right,
+        where matches exist
+    '''
+
+    # Create dataframes
+    df_left = pd.DataFrame(
+        index=pd.MultiIndex.from_arrays(
+            [
+                [0, 1, 2, 3, 4],
+                [5, 6, 7, 8, 9],
+            ],
+        ),
+        data={
+            'col_a': ['one', 'two', 'three', 'four', 'five'],
+            'col_b': [1, 2, 3, 4, 5]
+        }
+    )
+    df_right = pd.DataFrame(
+        index=pd.MultiIndex.from_arrays(
+            [
+                [0, 1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10, 11],
+            ],
+        ),
+        data={
+            'col_a': ['one', 'too', 'three', 'fours', 'five', 'five'],
+            'col_b': ['a', 'b', 'c', 'd', 'e', 'f']
+        }
+    )
+
+    # Use function
+    df_matches = fuzzy_match(
+        df_left,
+        df_right,
+        'col_a',
+        'col_a',
+        score_cutoff=60,
+        limit=2
+    )
+
+    # Add expected output
+    df_expected = pd.DataFrame(
+        index=pd.MultiIndex.from_arrays(
+            [
+                [(0, 5), (1, 6), (2, 7), (3, 8), (4, 9), (4, 9)],
+                [(0, 6), (1, 7), (2, 8), (3, 9), (4, 10), (5, 11)]
+            ],
+            names=['df_left_id', 'df_right_id']
+        ),
+        data={
+            'match_string': ['one', 'too', 'three', 'fours', 'five', 'five'],
+            'match_score': [100.000000, 66.666667, 100.000000, 88.888889, 100.000000, 100.000000],
+        }
+    )
+
+    # Test output
+    pdt.assert_frame_equal(df_matches, df_expected)
+
+    return
+
 def test_clean_strings_false():
     '''tests/test_fuzzy_match.py
         Test non-empty, non-MultiIndex df_left featuring punctuation,
