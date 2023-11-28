454733534c4c8853f21605eb0d45e29ff8d406ed
philipnye <philipnye@users.noreply.github.com>
2023-11-28 15:17:55 +0000

    Add calculate_date_string_end_date() + test

---
+++ tests/test_datetime_operations.py
@@ -0,0 +1,70 @@
+# #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+import pandas.testing as pdt
+
+from utils import datetime_operations as do
+
+
+def test_calculate_date_string_end_date():
+    data = [
+        ('2021-01-01', '%Y-%m-%d', pd.to_datetime('2021-01-01')),
+        ('2022-06-01', '%Y-%m-%d', pd.to_datetime('2022-06-01')),
+        ('01/01/2021', '%d/%m/%Y', pd.to_datetime('2021-01-01')),
+        ('01/06/2022', '%d/%m/%Y', pd.to_datetime('2022-06-01')),
+        ('31/03/2022', '%d/%m/%Y', pd.to_datetime('2022-03-31')),
+        ('2022', '%Y', pd.to_datetime('2022-12-31')),
+        ('2022/23', '%Y/%y', pd.to_datetime('2023-03-31')),
+        ('2022-23', '%Y-%y', pd.to_datetime('2023-08-31')),
+        ('Jan 2022 - Jan 2023', '%b %y - %b %y', pd.to_datetime('2023-01-31')),
+        ('Feb 2022 - Jan 2023', '%b %y - %b %y', pd.to_datetime('2023-01-31')),
+        ('February 2022 - February 2023', '%B %Y - %B %Y', pd.to_datetime('2023-02-28')),
+        ('February 2022 - January 2023', '%B %Y - %B %Y', pd.to_datetime('2023-01-31')),
+        ('April 2022/23', '%B %Y/%y', pd.to_datetime('2022-04-30')),
+        ('August 2022/23', '%B %Y/%y', pd.to_datetime('2022-08-31')),
+        ('March 2022/23', '%B %Y/%y', pd.to_datetime('2023-03-31')),
+        ('2023 Q1', '%Y %Q', pd.to_datetime('2023-03-31')),
+        ('2023 Q4', '%Y %Q', pd.to_datetime('2023-12-31')),
+        ('2022/23 Q3', '%Y/%y %Q', pd.to_datetime('2022-12-31')),
+        ('2022/23 Q4', '%Y/%y %Q', pd.to_datetime('2023-03-31')),
+        ('2023', '%Y + 1', pd.to_datetime('2023-12-31')),
+        ('31-03-2022', '31-03-%Y', pd.to_datetime('2022-03-31')),
+    ]
+
+    df = pd.DataFrame(
+        data,
+        columns=['input', 'format', 'output']
+    )
+
+    output = [
+        do.calculate_date_string_end_date(
+            input,
+            format,
+            financial_year_sep='/',
+            academic_year_sep='-',
+        )
+        for input, format
+        in zip(
+            df['input'],
+            df['format']
+        )
+    ]
+
+    pdt.assert_series_equal(df['output'], pd.Series(output, name='output'))
+
+    return
+
+
+def test_map_year_month_to_financial_year():
+    assert do.map_year_month_to_financial_year(2021, 4) == '2021/22'
+    assert do.map_year_month_to_financial_year(2021, 'April') == '2021/22'
+    assert do.map_year_month_to_financial_year(2021, 3) == '2020/21'
+    assert do.map_year_month_to_financial_year(2021, 'March') == '2020/21'
+    assert do.map_year_month_to_financial_year(2022, 1) == '2021/22'
+    assert do.map_year_month_to_financial_year(2022, 'January') == '2021/22'
+    assert do.map_year_month_to_financial_year(2022, 12) == '2022/23'
+    assert do.map_year_month_to_financial_year(2022, 'December') == '2022/23'
+
+    return
--- utils/datetime_operations.py
+++ utils/datetime_operations.py
@@ -1,9 +1,201 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import Union
+from dateutil.relativedelta import relativedelta
+from typing import Literal, Optional, Union
 
 import pandas as pd
+
+
+def calculate_date_string_end_date(
+    date_str: str,
+    date_format: Optional[str],
+    financial_year_sep: Literal['/', '-', None],
+    academic_year_sep: Literal['/', '-', None],
+) -> pd.Timestamp:
+    '''
+    Calculate an end date from a date string, including non-standard
+    date formats
+
+    Parameters:
+        - date_str (str): An input date strings - which can include
+        things in non-standard formats, such as calendar, financial
+        and academic year quarters
+        - date_format (str): The format the supplied date is in
+        - financial_year_sep (str): The separator between the two
+        years in a financial year
+        - academic_year_sep (str): The separator between the two
+        years in an academic year
+
+    Returns:
+        - end_date (datetime): The end date of the input date string
+
+    Notes:
+        None
+    '''
+
+    def predict_date_format(date_str):
+        '''
+        Attempt to predict the format of a date string based on its
+        formatting
+
+        Parameters:
+            - date_str (str): An input date strings - which can include
+            things in non-standard formats, such as calendar, financial
+            and academic year quarters
+
+        Returns:
+            - date_format (str): The end date of the input date string
+
+        Notes:
+            None
+        '''
+
+        if (
+            len(date_str) == 7 and
+            '/' in date_str and
+            len(date_str.split('/')[0]) == 4 and
+            len(date_str.split('/')[1]) == 2
+        ):
+            date_format = '%Y/%y'
+        else:
+            date_format = None
+
+        return date_format
+
+    # Attempt to predict date format
+    if date_format is None:
+        date_format = predict_date_format(date_str)
+
+    # Calendar year
+    if date_format in ['%Y', '%Y + 1']:
+        end_date = str(date_str) + '-12-31'
+
+    # Financial year
+    elif date_format == '%Y/%y' and financial_year_sep == '/':
+        end_date = date_str[:2] + date_str[-2:] + '-03-31'
+    elif date_format == '%Y-%y' and financial_year_sep == '-':
+        end_date = date_str[:2] + date_str[-2:] + '-03-31'
+
+    # Academic year
+    elif date_format == '%Y-%y' and academic_year_sep == '-':
+        end_date = date_str[:2] + date_str[-2:] + '-08-31'
+    elif date_format == '%Y/%y' and academic_year_sep == '/':
+        end_date = date_str[:2] + date_str[-2:] + '-08-31'
+
+    # Calendar year quarter
+    elif date_format == '%Y %Q':
+        if date_str[-3:] == 'All':
+            end_date = date_str[:4] + '-12-31'
+        elif date_str[-1:] == '1':
+            end_date = date_str[:4] + '-03-31'
+        elif date_str[-1:] == '2':
+            end_date = date_str[:4] + '-06-30'
+        elif date_str[-1:] == '3':
+            end_date = date_str[:4] + '-09-30'
+        elif date_str[-1:] == '4':
+            end_date = date_str[:4] + '-12-31'
+
+    # Financial year quarter
+    elif date_format in ['%Y/%y %Q', '%Y-%y %Q']:
+        if date_str[-3:] == 'All':
+            end_date = str(int(date_str[:4]) + 1) + '-03-31'
+        elif date_str[-1:] == '4':
+            end_date = str(int(date_str[:4]) + 1) + '-03-31'
+        elif date_str[-1:] == '1':
+            end_date = date_str[:4] + '-06-30'
+        elif date_str[-1:] == '2':
+            end_date = date_str[:4] + '-09-30'
+        elif date_str[-1:] == '3':
+            end_date = date_str[:4] + '-12-31'
+
+    # School term
+    # NB: This calculates a notional rather than an actual end
+    # date - e.g. April will not always fall in the spring term
+    elif date_format == '%Y-%y %T':
+        if 'autumn' in date_str.lower():
+            end_date = date_str[:4] + '-12-31'
+        elif 'spring' in date_str.lower():
+            end_date = date_str[:2] + date_str[5:7] + '-04-30'
+        elif 'summer' in date_str.lower():
+            end_date = date_str[:2] + date_str[5:7] + '-08-31'
+
+    # 31 March of year
+    elif date_format == '31-03-%Y':
+        end_date = pd.to_datetime(
+            date_str,
+            dayfirst=True
+        )
+
+    # Other non-standard formats
+    elif date_format == '%b %y - %b %y':
+
+        # Turn end month/year into a date, add a month and subtract a day -
+        # as the easiest way of getting to the end of the month
+        end_date = (
+            pd.to_datetime(date_str.split('-')[1].strip()) +
+            relativedelta(months=1) - relativedelta(days=1)
+        )
+
+    elif date_format == '%B %Y - %B %Y':
+
+        # Turn end month/year into a date, add a month and subtract a day -
+        # as the easiest way of getting to the end of the month
+        end_date = (
+            pd.to_datetime(date_str.split('-')[1].strip()) +
+            relativedelta(months=1) - relativedelta(days=1)
+        )
+
+    elif date_format == '%B %Y':
+        end_date = (
+            pd.to_datetime(date_str) +
+            relativedelta(months=1) - relativedelta(days=1)
+        )
+
+    elif date_format == '%b-%Y':
+        end_date = (
+            pd.to_datetime(date_str) +
+            relativedelta(months=1) - relativedelta(days=1)
+        )
+
+    elif date_format == '%b-%y':
+        date_str = date_str[:3] + '-20' + date_str[-2:]
+        end_date = (
+            pd.to_datetime(date_str) +
+            relativedelta(months=1) - relativedelta(days=1)
+        )
+
+    elif date_format in ['%B %Y/%y', '%B %Y-%y']:
+
+        # Grab month + first year of financial year
+        end_date = pd.to_datetime(date_str[:-3])
+
+        # Iterate year if we're in the first three months
+        if end_date.month <= 3:
+            end_date += relativedelta(years=1)
+
+        # Move to end of the month
+        end_date += relativedelta(months=1) - relativedelta(days=1)
+
+    # Standard formats
+    elif date_format in [
+        '%d/%m/%Y',
+        '%d-%b-%Y',
+        '%Y-%m-%d',
+    ]:
+        end_date = pd.to_datetime(
+            date_str,
+            format=date_format
+        )
+
+    else:
+        raise RuntimeError('Date format not handled')
+
+    end_date = pd.to_datetime(
+        end_date
+    )
+
+    return end_date
 
 
 def convert_academicfinancial_year_string_to_year_string(year: str) -> str:
