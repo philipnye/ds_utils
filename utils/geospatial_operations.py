2cd75acaa7a866f3b1309c7534555126644e9474
philipnye <philipnye@users.noreply.github.com>
2023-12-05 20:59:29 +0000

    - Tweak shapely import - Accept kwargs in merge_geometries()

--- utils/geospatial_operations.py
+++ utils/geospatial_operations.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from shapely.geometry import MultiPolygon, Polygon
-from shapely.ops import unary_union
+from shapely import unary_union
 
 
 def merge_geometries(
     geometries: list[Polygon, MultiPolygon],
-    exterior_only: bool = True
+    exterior_only: bool = True,
+    **kwargs
 ):
     """
     Merge a list of geometries into a single geometry, optionally returning only
@@ -17,6 +18,7 @@ def merge_geometries(
         - geometries: List of geometries (Polygons MultiPolygons) to merge
         - exterior_only: Whether to return only the exterior coordinates of the
         merged geometry
+        - kwargs: Additional keyword arguments to pass to unary_union()
 
     Returns
         - merged_geometry: Merged geometry
@@ -26,7 +28,7 @@ def merge_geometries(
         each geometry in the MultiPolygon are returned
     """
 
-    merged_geometry = unary_union(geometries)
+    merged_geometry = unary_union(geometries, **kwargs)
 
     if exterior_only:
         if merged_geometry.geom_type == 'Polygon':
