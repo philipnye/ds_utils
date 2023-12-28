9035f017f26ba36683a7d727a14d5d9902b6e56d
philipnye <philipnye@users.noreply.github.com>
2023-12-28 10:41:12 +0000

    Implement split_title_names()

--- ds_utils/string_operations.py
+++ ds_utils/string_operations.py
@@ -1,6 +1,63 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from typing import Optional
+
+
+def split_title_names(
+    name: str,
+) -> tuple[Optional[str], Optional[str], Optional[str]]:
+    """
+    Split names containing 'of' into probable title, last_name, place
+
+    Parameters
+        - name: Name to split
+
+    Returns
+        - title: Probable title
+        - last_name: Probable last name
+        - place: Probable place
+
+    Notes
+        None
+    """
+    # Raise error if 'of' not in name
+    if 'of' not in name:
+        raise ValueError('\'of\' not in name')
+
+    # Set default values
+    title, last_name, place = None, None, None
+
+    # Count number of words before 'of'
+    name_split = name.split(' ')
+    of_index = name_split.index('of')
+
+    # If one word before 'of', word before 'of' is title, words after 'of' are place
+    # e.g. 'Duke of Wellington', 'Earl of Minto'
+    if of_index == 1:
+        title = name_split[0]
+        place = ' '.join(name_split[of_index + 1:])
+
+    # If 'Lord Archbishop' or 'Lord Bishop' before 'of', words before 'of' are title,
+    # words after 'of' are place
+    # e.g. 'Lord Bishop of London', 'Lord Bishop of Bath and Wells'
+    elif (
+        of_index > 1 and
+        ' '.join(name_split[:2]) in ['Lord Archbishop', 'Lord Bishop']
+    ):
+        title = ' '.join(name_split[:2])
+        place = ' '.join(name_split[3:])
+
+    # If more than one word before 'of', first word is title, other words before
+    # 'of' are last_name, and words after 'of' are place
+    # e.g. 'Lord Young of Cookham', 'Lord Cameron of Chipping Norton'
+    elif of_index > 1:
+        title = name_split[0]
+        last_name = ' '.join(name_split[1:of_index])
+        place = ' '.join(name_split[of_index + 1:])
+
+    return title, last_name, place
+
 
 def strip_name_title(
     name: str,
